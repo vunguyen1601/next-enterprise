2eef30d9973011469820b7e26fe333ffe1c47027
Jakub Jablonski <jjablonski.it@gmail.com>
2023-05-03 19:16:11 +0200

    refactor: change next config file to mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,10 @@
 import withBundleAnalyzer from "@next/bundle-analyzer"
 import withPlugins from "next-compose-plugins"
-import { env } from "./env.mjs"
 
 /**
  * @type {import('next').NextConfig}
  */
-const config = withPlugins([[withBundleAnalyzer({ enabled: env.ANALYZE })]], {
+const config = withPlugins([[withBundleAnalyzer({ enabled: process.env.ANALYZE === "true" })]], {
   reactStrictMode: true,
   experimental: { instrumentationHook: true },
   rewrites() {
